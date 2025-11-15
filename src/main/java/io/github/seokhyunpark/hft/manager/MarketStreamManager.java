b7304b4ad49b9edd4f2a7c3b683a1efe2d8e0eb9
skhnpark <skhnpark125@gmail.com>
2025-11-15 21:11:44 +0900

    feat: MarketStreamManager와 UserStreamManager가 BinanceConnector를 주입받는 기능 구현

--- src/main/java/io/github/seokhyunpark/hft/manager/MarketStreamManager.java
+++ src/main/java/io/github/seokhyunpark/hft/manager/MarketStreamManager.java
@@ -2,8 +2,14 @@ package io.github.seokhyunpark.hft.manager;
 
 import com.binance.connector.client.common.websocket.service.StreamBlockingQueueWrapper;
 import com.binance.connector.client.spot.websocket.stream.model.PartialBookDepthResponse;
+import io.github.seokhyunpark.hft.connector.BinanceConnector;
 
 public class MarketStreamManager {
+    private final BinanceConnector binanceConnector;
+
+    public MarketStreamManager(BinanceConnector binanceConnector) {
+        this.binanceConnector = binanceConnector;
+    }
 
     public void startProcessing(StreamBlockingQueueWrapper<PartialBookDepthResponse> queue) {
         System.out.println("[ManagerStream] 데이터 처리 시작. 큐 대기 중...");
